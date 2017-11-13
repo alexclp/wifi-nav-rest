bcbc447bc658f815338cfce756ba77990625ef93
alexclp <alexandru.clapa@gmail.com>
2017-11-13 13:10:14 +0000

    Start implementing methods form RoomController

--- Sources/App/Controllers/RoomController.swift
+++ Sources/App/Controllers/RoomController.swift
@@ -1,8 +1,15 @@
-// import Vapor
-// import HTTP
-//
-// final class RoomController: ResourceRepresentable {
-//
-// }
-//
-// extension RoomController: EmptyInitializable { }
+import Vapor
+import HTTP
+
+final class RoomController: ResourceRepresentable {
+
+}
+
+extension RoomController: EmptyInitializable { }
+
+extension Request {
+    func createRoom() throws -> Room {
+        guard let json = json else { throw Abort.badRequest }
+        return try Room(json: json)
+    }
+}
