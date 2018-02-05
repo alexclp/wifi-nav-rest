4de775df65e8c64b238dc5cd5452306fc1b4a721
alexclp <alexandru.clapa@gmail.com>
2018-02-05 14:14:17 +0000

    Disabling some routes for now

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -20,38 +20,38 @@ extension Droplet {
 
         get("description") { req in return req.description }
 
-        get("accessPoints", "address", ":macAddress") { request in
-            guard let macAddress = request.parameters["macAddress"]?.string else { throw Abort.badRequest }
-            guard let ap = try WiFiAP.makeQuery().filter("macAddress", .equals, macAddress).first() else { throw Abort.notFound }
-            return try ap.makeJSON()
-        }
+        // get("accessPoints", "address", ":macAddress") { request in
+        //     guard let macAddress = request.parameters["macAddress"]?.string else { throw Abort.badRequest }
+        //     guard let ap = try WiFiAP.makeQuery().filter("macAddress", .equals, macAddress).first() else { throw Abort.notFound }
+        //     return try ap.makeJSON()
+        // }
 
-        get("rooms", "floor", ":floorNumber") { request in
-            guard let floorNumber = request.parameters["floorNumber"]?.int else { throw Abort.badRequest }
-            let rooms = try Room.makeQuery().filter("floorNumber", .equals, floorNumber).all()
-            return try rooms.makeJSON()
-        }
+        // get("rooms", "floor", ":floorNumber") { request in
+        //     guard let floorNumber = request.parameters["floorNumber"]?.int else { throw Abort.badRequest }
+        //     let rooms = try Room.makeQuery().filter("floorNumber", .equals, floorNumber).all()
+        //     return try rooms.makeJSON()
+        // }
 
-        get("measurements", "address", ":macAddress") { request in 
-            guard let macAddress = request.parameters["macAddress"]?.string else { throw Abort.badRequest }
-            guard let accessPoint = try WiFiAP.makeQuery().filter("macAddress", .equals, macAddress).first() else { throw Abort.notFound }
-            let measurements = try Measurement.makeQuery().filter("apID", .equals, accessPoint.id).all()
-            return try measurements.makeJSON()
-        }
+        // get("measurements", "address", ":macAddress") { request in 
+        //     guard let macAddress = request.parameters["macAddress"]?.string else { throw Abort.badRequest }
+        //     guard let accessPoint = try WiFiAP.makeQuery().filter("macAddress", .equals, macAddress).first() else { throw Abort.notFound }
+        //     let measurements = try Measurement.makeQuery().filter("apID", .equals, accessPoint.id).all()
+        //     return try measurements.makeJSON()
+        // }
 
-        delete("rooms", "clearData", ":id") { request in
-            guard let roomID = request.parameters["id"]?.int else { throw Abort.badRequest }
-            guard let room = try Room.find(roomID) else { throw Abort.notFound }
-            let locations = try Location.makeQuery().filter("roomID", .equals, roomID).all()
-            for location in locations {
-                let measurements = try Measurement.makeQuery().filter("locationID", .equals, location.id).all()
-                for measurement in measurements {
-                    try measurement.delete()
-                }
-                try location.delete()
-            }
-            return "{ \"success\": true }"
-        }
+        // delete("rooms", "clearData", ":id") { request in
+        //     guard let roomID = request.parameters["id"]?.int else { throw Abort.badRequest }
+        //     guard let room = try Room.find(roomID) else { throw Abort.notFound }
+        //     let locations = try Location.makeQuery().filter("roomID", .equals, roomID).all()
+        //     for location in locations {
+        //         let measurements = try Measurement.makeQuery().filter("locationID", .equals, location.id).all()
+        //         for measurement in measurements {
+        //             try measurement.delete()
+        //         }
+        //         try location.delete()
+        //     }
+        //     return "{ \"success\": true }"
+        // }
 
         try resource("locations", LocationController.self)
         try resource("rooms", RoomController.self)
